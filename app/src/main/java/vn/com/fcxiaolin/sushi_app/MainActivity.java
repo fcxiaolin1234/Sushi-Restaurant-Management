a869daa30e82c173e9d3e7cddda5caa9871ae190
duongcopeocp <duongcopeozz@gmail.com>
2018-01-08 22:50:27 +0700

    a

--- app/src/main/java/vn/com/fcxiaolin/sushi_app/MainActivity.java
+++ app/src/main/java/vn/com/fcxiaolin/sushi_app/MainActivity.java
@@ -1,13 +1,34 @@
 package vn.com.fcxiaolin.sushi_app;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drLayout;
+    private ActionBarDrawerToggle drToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drLayout=(DrawerLayout) findViewById(R.id.drawerlayout);
+        drToggle=new ActionBarDrawerToggle(this,drLayout,R.string.nav_open,R.string.nav_close);
+       drToggle.syncState();
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(drToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
+
+
