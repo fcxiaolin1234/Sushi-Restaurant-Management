f16aae5671e29fa770b29c1205a14d1e58fdde5e
duongcopeocp <duongcopeozz@gmail.com>
2018-01-15 22:27:36 +0700

    update front navigation

--- app/src/main/java/vn/com/fcxiaolin/sushi_app/navigation_screen.java
+++ app/src/main/java/vn/com/fcxiaolin/sushi_app/navigation_screen.java
@@ -1,23 +1,34 @@
 package vn.com.fcxiaolin.sushi_app;
 
+import android.drm.DrmStore;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import android.support.v7.widget.Toolbar;
+
+public class navigation_screen extends AppCompatActivity {
     private DrawerLayout drLayout;
     private ActionBarDrawerToggle drToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_screen);
         drLayout=(DrawerLayout) findViewById(R.id.drawerlayout);
-        drToggle=new ActionBarDrawerToggle(this,drLayout,R.string.nav_open,R.string.nav_close);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drToggle=new ActionBarDrawerToggle(this,drLayout,toolbar,R.string.nav_open,R.string.nav_close);
+        drLayout.setDrawerListener(drToggle);
        drToggle.syncState();
-       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
 
     }
     @Override
